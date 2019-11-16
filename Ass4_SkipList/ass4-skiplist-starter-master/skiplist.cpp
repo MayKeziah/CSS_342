5b20281f88d9f96c44f597051d9fc2f042a069c3
MayKeziah <keziah.may@gmail.com>
2019-11-16 14:57:42 -0800

    Finished SkipList toString
    TODO: Consider how to make more efficient

--- Ass4_SkipList/ass4-skiplist-starter-master/skiplist.cpp
+++ Ass4_SkipList/ass4-skiplist-starter-master/skiplist.cpp
@@ -102,9 +102,12 @@ void SkipList::setGuards(){
 // given a string array with matching depth, converts each level
 // to a string and stores it in the associated string array index.
 void SkipList::toString(string* Levels) const{
-    SNode* Current = FrontGuards[0];
-    while(Current != nullptr){
-        Levels[0] += Current;
+    for (int I = 0; I < Depth; I++){
+        SNode* Current = FrontGuards[I];
+        while(Current != RearGuards[I]){
+            Levels[I] += Current->toString() + "<->";
+            Current = Current->Next;
+        } Levels[I] += Current->toString();
     }
 
 }
