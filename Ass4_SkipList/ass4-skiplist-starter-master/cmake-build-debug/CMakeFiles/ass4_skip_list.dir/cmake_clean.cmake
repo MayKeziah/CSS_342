d2da1046501d0e11b0389214909dd013f52455ff
MayKeziah <keziah.may@gmail.com>
2019-11-16 09:25:25 -0800

    Downloaded Starter Zip
    Downloaded, extracted, prepped main for method testing during development, commented TODO on a line form starter code that I do not understand.

---
+++ Ass4_SkipList/ass4-skiplist-starter-master/cmake-build-debug/CMakeFiles/ass4_skip_list.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ass4_skip_list.dir/main.cpp.o"
+  "CMakeFiles/ass4_skip_list.dir/skiplist.cpp.o"
+  "ass4_skip_list.exe"
+  "ass4_skip_list.pdb"
+  "libass4_skip_list.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ass4_skip_list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Ass4_SkipList/ass4-skiplist-starter-master/cmake-build-debug/CMakeFiles/ass4_skip_list.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ass4_skip_list.
+# This may be replaced when dependencies are built.
